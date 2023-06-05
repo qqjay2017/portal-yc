ba0933dce4f505a160c196be7d2d8c763bb0c14b
huangbo <huangbo@lj-ch.com>
2023-06-05 22:34:22 +0800

    feat: 修改111

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <RoomCanvas />
+      {/* <RoomCanvas /> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
