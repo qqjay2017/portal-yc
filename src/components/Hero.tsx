b89a7946aaf50caa9694410ac934c1eded9f7aeb
huangbo <huangbo@lj-ch.com>
2023-06-08 21:43:57 +0800

    feat: 修改模型111

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
+import { RoomCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -24,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <RoomCanvas /> */}
+      <RoomCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
--- src/components/canvas/Room.tsx
+++ src/components/canvas/Room.tsx
@@ -2,104 +2,164 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { Suspense, useEffect, useState } from "react";
 import CanvasLoader from "../Loader";
-import * as THREE from "three";
 
 const Room = ({ isMobile }: { isMobile?: boolean }) => {
-  console.log(isMobile, "isMobile");
-  const room: any = useGLTF("./MonsantoHouse/merge.gltf");
-  const floorColorMap = useLoader(
-    THREE.TextureLoader,
-    "/floor_tiles_06/FloorsCheckerboard_S_Diffuse.jpg"
-  );
-  const floorNormalMap = useLoader(
-    THREE.TextureLoader,
-    "/floor_tiles_06/FloorsCheckerboard_S_Normal.jpg"
-  );
+  const room: any = useGLTF("./MonsantoHouse/001.gltf");
 
-  const { nodes } = room;
-
-  const FlowMeshPhongMaterial = (
-    <meshPhongMaterial
-      side={THREE.DoubleSide}
-      map={floorColorMap}
-      normalMap={floorNormalMap}
-    ></meshPhongMaterial>
-  );
-  const GlassMeshPhysicalMaterial = (
-    <meshPhysicalMaterial
-      color={0xffffff}
-      transmission={0.5}
-      opacity={1}
-      metalness={0}
-      roughness={0}
-      ior={1.52}
-      thickness={0.8}
-      specularIntensity={1}
-      specularColor="#ffffff"
-      lightMapIntensity={1}
-    ></meshPhysicalMaterial>
-  );
+  const { nodes, materials } = room;
 
   return (
-    <group dispose={null}>
-      <mesh geometry={nodes.model_1.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_0.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_2.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_3.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_4.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_5.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_6.geometry} rotation={[Math.PI / 2, 0, 0]} />
+    <group dispose={null} position={[0, -1, 0]}>
+      <mesh
+        geometry={nodes.model_0.geometry}
+        material={materials["Default OBJ.002"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_1.geometry}
+        material={materials["Default OBJ.002"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_2.geometry}
+        material={materials["Default OBJ.002"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_3.geometry}
+        material={materials["Default OBJ.002"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_4.geometry}
+        material={materials["Default OBJ.005"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_5.geometry}
+        material={materials["Default OBJ.005"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_6.geometry}
+        material={materials["Default OBJ.005"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
       <mesh
         geometry={nodes.model_7.geometry}
+        material={materials["Default OBJ.005"]}
         rotation={[Math.PI / 2, 0, 0]}
-      ></mesh>
-      <mesh geometry={nodes.model_8.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_9.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_10.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_11.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {FlowMeshPhongMaterial}
-      </mesh>
+      />
+      <mesh
+        geometry={nodes.model_8.geometry}
+        material={materials["Default OBJ.005"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_9.geometry}
+        material={materials["Default OBJ.009"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_10.geometry}
+        material={materials["Default OBJ.010"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_11.geometry}
+        material={materials["Default OBJ.011"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
       <mesh
         geometry={nodes.model_12.geometry}
+        material={materials["Default OBJ.012"]}
         rotation={[Math.PI / 2, 0, 0]}
-      ></mesh>
-      <mesh geometry={nodes.model_13.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {FlowMeshPhongMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_14.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_15.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_16.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {FlowMeshPhongMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_17.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {FlowMeshPhongMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_18.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {FlowMeshPhongMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_19.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_20.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_21.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_22.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_23.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {FlowMeshPhongMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_24.geometry} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.model_25.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {FlowMeshPhongMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_26.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {FlowMeshPhongMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_27.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {GlassMeshPhysicalMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_28.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {GlassMeshPhysicalMaterial}
-      </mesh>
-      <mesh geometry={nodes.model_29.geometry} rotation={[Math.PI / 2, 0, 0]}>
-        {GlassMeshPhysicalMaterial}
-      </mesh>
+      />
+      <mesh
+        geometry={nodes.model_13.geometry}
+        material={materials["Default OBJ.014"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_14.geometry}
+        material={materials["Default OBJ.015"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_15.geometry}
+        material={materials["Default OBJ.009"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_16.geometry}
+        material={materials["Default OBJ.017"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_17.geometry}
+        material={materials["Default OBJ.018"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_18.geometry}
+        material={materials["Default OBJ.019"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_19.geometry}
+        material={materials["Default OBJ.020"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_20.geometry}
+        material={materials["Default OBJ.031"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_21.geometry}
+        material={materials["Default OBJ.022"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_22.geometry}
+        material={materials["Default OBJ.023"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_23.geometry}
+        material={materials["Default OBJ.024"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_24.geometry}
+        material={materials["Default OBJ.025"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_25.geometry}
+        material={materials["Default OBJ.026"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_26.geometry}
+        material={materials["Default OBJ.027"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_27.geometry}
+        material={materials["Default OBJ.021"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_28.geometry}
+        material={materials["Default OBJ.030"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        geometry={nodes.model_29.geometry}
+        material={materials["Default OBJ.030"]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
     </group>
   );
 };
@@ -132,9 +192,10 @@ const RoomCanvas = () => {
       frameloop="demand"
       shadows
       dpr={[1, 2]}
-      camera={{ position: [6, 1, 2], fov: 75 }}
+      camera={{ position: [6, -6, 2], fov: 75 }}
       gl={{ preserveDrawingBuffer: true }}
     >
+      <ambientLight castShadow intensity={0.2} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -143,8 +204,17 @@ const RoomCanvas = () => {
         castShadow
         shadow-mapSize={1024}
       />
+      <spotLight
+        position={[20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
@@ -152,7 +222,7 @@ const RoomCanvas = () => {
         <Room isMobile={isMobile} />
       </Suspense>
 
-      <Preload all />
+      {/* <Preload all /> */}
     </Canvas>
   );
 };
