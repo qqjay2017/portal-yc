7abd478ed622eee2ba487370a2c8141a724a62fa
huangbo <huangbo@lj-ch.com>
2023-06-09 21:45:40 +0800

    feat: 修改标题1111

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -13,7 +13,7 @@ const ProjectCard = ({
   description,
   tags,
   image,
-  source_code_link,
+  // source_code_link,
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
@@ -31,13 +31,6 @@ const ProjectCard = ({
             alt="project_image"
             className="w-full h-full object-cover rounded-2xl"
           />
-
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            ></div>
-          </div>
         </div>
 
         <div className="mt-5">
