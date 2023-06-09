db6a50d85026bdd8adcfa81c688179d0027108d5
huangbo <huangbo@lj-ch.com>
2023-06-09 21:41:49 +0800

    feat: 替换模型11111

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,7 +3,7 @@ import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
-import github from "./github.png";
+
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -37,7 +37,6 @@ export {
   creator,
   mobile,
   web,
-  github,
   menu,
   close,
   css,
